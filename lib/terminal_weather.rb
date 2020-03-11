62ed360a06092a4d7db8ecf36b686806f260d37e
Danny <dannylow465@gmail.com>
2020-03-11 11:55:07 +1100

    pushing gem

--- lib/terminal_weather.rb
+++ lib/terminal_weather.rb
@@ -1,6 +1,17 @@
-require "terminal_weather/version"
+require "open-uri"
+require "json"
+require "ipaddr"
+require "geocoder"
 
-module TerminalWeather
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# result = Geocoder.search("busselton, australia")
+# results = result.first.coordinates
+# pp results
+# x = results[0]
+# y = results[1]
+
+# weather_call = URI("https://api.darksky.net/forecast/b90bba0f6d3f8c2e3102c9b691f4803d/#{x},#{y}").open.read
+
+# pp weather_call
+v = IPSocket.getaddress 'broadcasthost'
+
+pp v
